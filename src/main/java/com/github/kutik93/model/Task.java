0583e5284c404397039f9ede702537bb315959af
Uzytkownik <kutik93@gmail.com>
2022-10-12 22:08:19 +0200

    Implement a simple validator. It was created for this purpose interface (TestRepository) and have been modified to class: Task and TodoAppApplication.

---
+++ src/main/java/com/github/kutik93/model/Task.java
@@ -0,0 +1,43 @@
+package com.github.kutik93.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @NotBlank(message = "Task's description must be not empty")
+    private String description;
+    private boolean done;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Task() {
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
