220986e2cad59cf8714b6239affa25efd8c4508c
Uzytkownik <kutik93@gmail.com>
2022-10-12 10:42:11 +0200

    Initial configuration of the Maven project. Preparation the environment for working on the todo list project.

---
+++ src/test/java/com/github/kutik93/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kutik93;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
