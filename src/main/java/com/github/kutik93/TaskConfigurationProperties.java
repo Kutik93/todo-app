cc5d7779cd117f3add6b6444654f784e5332e3bc
Uzytkownik <kutik93@gmail.com>
2022-10-19 11:07:21 +0200

    Profiles have been added.

--- src/main/java/com/github/kutik93/TaskConfigurationProperties.java
+++ src/main/java/com/github/kutik93/TaskConfigurationProperties.java
@@ -8,10 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class TaskConfigurationProperties {
     private TaskTemplate taskTemplate;
 
-    public TaskConfigurationProperties(TaskTemplate taskTemplate) {
-        this.taskTemplate = taskTemplate;
-    }
-
     public TaskTemplate getTemplate() {
         return taskTemplate;
     }
