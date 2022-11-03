4c9aad11f4f9d88fe1c544ffe78500cfd9f4c2ef
Uzytkownik <kutik93@gmail.com>
2022-11-03 12:29:52 +0100

    Added errors messages.

--- src/main/java/com/github/kutik93/controller/ProjectController.java
+++ src/main/java/com/github/kutik93/controller/ProjectController.java
@@ -6,11 +6,13 @@ import com.github.kutik93.model.ProjectStep;
 import com.github.kutik93.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -29,7 +31,13 @@ class ProjectController {
     }
 
     @PostMapping
-    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model){
+    String addProject(
+            @ModelAttribute("project") @Valid ProjectWriteModel current,
+            BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "projects";
+        }
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
         model.addAttribute("message", "Dodano projekt!");
@@ -43,7 +51,7 @@ class ProjectController {
     }
 
     @ModelAttribute("projects")
-    List<Project> getProjects(){
+    List<Project> getProjects() {
         return service.readAll();
     }
 }
